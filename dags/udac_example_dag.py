1183366e2d052a8ecfd1edbc113d5216341a0622
mchez808 <mark.chesney@gmail.com>
2020-12-20 16:06:18 -0800

    update with udacity template folder

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -34,42 +34,6 @@ stage_songs_to_redshift = StageToRedshiftOperator(
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
-from datetime import datetime, timedelta
-import os
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator)
-from helpers import SqlQueries
-
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
-
-default_args = {
-    'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
-}
-
-dag = DAG('udac_example_dag',
-          default_args=default_args,
-          description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
-        )
-
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
-
-stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
-    dag=dag
-)
-
-stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
-    dag=dag
-)
-
-load_songplays_table = LoadFactOperator(
-    task_id='Load_songplays_fact_table',
     dag=dag
 )
 
