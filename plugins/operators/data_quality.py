8a864f55cd60fe0b6ad0554cdf493d144302579c
mchez808 <mark.chesney@gmail.com>
2020-12-22 16:00:03 -0800

    add instructions as comments

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -20,3 +20,16 @@ class DataQualityOperator(BaseOperator):
 
     def execute(self, context):
         self.log.info('DataQualityOperator not implemented yet')
+        
+
+
+## Data Quality Operators
+# The final operator to create is the data quality operator, which is used to run checks on the data itself.
+# The operator's main functionality is to receive
+#     - one or more SQL based test cases along with
+#     - the expected results
+# and execute the tests.
+# For each test, the test result and expected result needs to be checked and
+# if there is no match, the operator should raise an exception and the task should retry and fail eventually.
+#
+# For example one test could be a SQL statement that checks if certain column contains NULL values by counting all the rows that have NULL in the column. We do not want to have any NULLs so expected result would be 0 and the test would compare the SQL statement's outcome to the expected result.
