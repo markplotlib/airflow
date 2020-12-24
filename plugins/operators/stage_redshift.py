5bb9e0a29ac799a5739023a919ba32fabcaa9c81
mchez808 <mark.chesney@gmail.com>
2020-12-23 19:41:51 -0800

    pass in table param

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -9,6 +9,7 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
     template_fields = ('s3_key',)
@@ -24,10 +25,9 @@ class StageToRedshiftOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
                  redshift_conn_id="",
                  aws_credentials_id="",
-                 #
+                 table="",
                  s3_bucket="",
                  s3_key="",
                  delimiter=",",
@@ -35,13 +35,13 @@ class StageToRedshiftOperator(BaseOperator):
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
+        self.table = table
         self.redshift_conn_id = redshift_conn_id
-        self.aws_credentials_id = aws_credentials_id
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
         self.delimiter = delimiter
         self.ignore_headers = ignore_headers
+        self.aws_credentials_id = aws_credentials_id
 
 
     def execute(self, context):
@@ -54,7 +54,7 @@ class StageToRedshiftOperator(BaseOperator):
         redshift = PostgresHook(postgres_conn_id=redshift_conn_id)
 
         self.log.info('Clearing data from destination Redshift table')
-        # TODO: run SQL DELETE command
+        redshift.run("DELETE FROM {}".format(self.table))
 
         self.log.info('Copying data from S3 to Redshift')
         rendered_key = self.s3_key.format(**context)
