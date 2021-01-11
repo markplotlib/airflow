d83a225f0783950922eeccefe8405b38f521543b
markplotlib <mark.chesney@gmail.com>
2021-01-11 09:06:12 -0800

    add default_args

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -14,7 +14,11 @@ AWS_SECRET = os.environ.get('AWS_SECRET')
 
 default_args = {
     'owner': 'udacity',
+    'depends_on_past': False,
     'start_date': datetime(2019, 1, 12),
+	'retries': 3,
+	'retry_delay': timedelta(minutes=5),
+	'catchup': False
 }
 
 dag = DAG('udac_example_dag',
