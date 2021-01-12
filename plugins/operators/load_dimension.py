93edcc6675afa035eec76edd19f6588d06a861b9
markplotlib <mark.chesney@gmail.com>
2021-01-12 13:45:08 -0800

    fulfill req: 'DAG allows to switch between append-only and delete-load functionality'

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -3,12 +3,7 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
-    dim_sql_template = """
-    DROP TABLE IF EXISTS {destination_table};
-    CREATE TABLE {destination_table} AS
-    {dim_table_query}
-    """
-
+    sql_template = ""
     ui_color = '#80BD9E'
 
     @apply_defaults
@@ -16,26 +11,40 @@ class LoadDimensionOperator(BaseOperator):
                  redshift_conn_id="",
                  dim_table_query="",
                  destination_table="",
+                 append_records=True,
                  *args, **kwargs):
 
         # this super(<SameClass>, self) call below is equivalent to
-        # the parameterless super() call, giving access to methods
-        # in a superclass from the subclass that inherits from it.
+        # the parameterless super() call, giving access to
+        # methods in a superclass to the inheriting subclass.
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id=redshift_conn_id
         self.destination_table=destination_table
         self.dim_table_query=dim_table_query
+        self.append_records=append_records
 
 
     def execute(self, context):
         # Fetch the redshift hook
         redshift = PostgresHook(postgres_conn_id=self.postgres_conn_id)
 
-        # Format the `dim_sql_template`
-        formatted_dim_sql = LoadDimensionOperator.dim_sql_template.format(
+        if append_records:
+            sql_template = """
+            INSERT INTO TABLE {destination_table} AS
+            {dim_table_query}
+            """
+        else:
+            sql_template = """
+            DROP TABLE IF EXISTS {destination_table};
+            CREATE TABLE {destination_table} AS
+            {dim_table_query}
+            """
+
+        # Format the SQL template
+        formatted_sql = LoadDimensionOperator.sql_template.format(
             destination_table=self.destination_table,
             dim_table_query=self.dim_table_query
         )
 
         # run the query against redshift
-        redshift.run(formatted_dim_sql)
+        redshift.run(formatted_sql)
