0abdb3fd22ae2b21ab0679b6d4c248ce6b788fea
markplotlib <mark.chesney@gmail.com>
2021-01-12 22:21:12 -0800

    set max_active_runs=1 in DAG param

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -23,6 +23,7 @@ default_args = {
 
 dag = DAG('udac_example_dag',
           default_args=default_args,
+          max_active_runs=1,
           description='Load and transform data in Redshift with Airflow',
           schedule_interval='@hourly'
         )
